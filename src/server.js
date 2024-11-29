cebf3cf2f3051b0148e439ab76cf98cbec91a2c2
anhoang0611 <94751169+anhoang0611@users.noreply.github.com>
2024-11-30 04:22:47 +0700

    ket noi db

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 const express = require('express')//commonjs
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
+
+const mysql = require('mysql2');
 // import express from 'express';
 
 
@@ -18,6 +20,25 @@ configViewEngine(app);
 
 //khai bao route
 app.use('/', webRoutes);
+
+//test connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307, // default: 3306
+    user: 'root',// defaut: empty
+    password: '123456',
+    database: 'hoidanit'
+
+});
+//simple query
+connection.query(
+    'SELECT * FROM Users u',
+    function (err, results, fields) {
+        console.log(">>>results= ", results);
+        console.log(">>>field= ", fields);
+    }
+)
+
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
