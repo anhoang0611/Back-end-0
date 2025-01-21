18a9497d42e3db57599c2d64cf08685601a3c3cb
anhoang0611 <94751169+anhoang0611@users.noreply.github.com>
2025-01-22 02:11:09 +0700

    tao id 1 va n project

--- src/services/productService.js
+++ src/services/productService.js
@@ -6,5 +6,19 @@ module.exports = {
             let result = await Project.create(data);
             return result;
         }
+        if (data.type === 'ADD-USER') {
+            console.log(">>> check data: ", data);
+            let myProject = await Project.findById(data.projectId).exec();
+
+            console.log(">>> check myProject: ", myProject);
+            for (let i = 0; i < data.usersArr.length; i++) {
+                myProject.userInfor.push(data.usersArr[i]);
+            }
+
+            let newResult = await myProject.save();
+
+            return newResult;
+        }
+        return null;
     }
 }
